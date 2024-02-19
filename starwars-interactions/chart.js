ebfb50b019c5869cb9ee059da1798c90bf086729
hamreliusfilip <91670593+hamreliusfilip@users.noreply.github.com>
2024-02-19 14:55:29 +0100

    Implemented zoom

--- starwars-interactions/chart.js
+++ starwars-interactions/chart.js
@@ -1,6 +1,26 @@
 function createChart(chartSelector, dataURL, width, height, centerPos) {
-    var chargeStrength = -150; 
-    var simulation; 
+    var chargeStrength = -150;
+    var simulation;
+    var zoom;
+
+    zoom = d3.zoom()
+        .scaleExtent([0.1, 4])
+        .on('zoom', zoomed);
+
+    d3.select(chartSelector + ' svg')
+        .call(zoom);
+
+    d3.select('#zoomSlider').on('input', function () {
+        var zoomLevel = +this.value;
+        d3.select('#zoomValue').text(zoomLevel);
+        d3.select(chartSelector + ' svg').call(zoom.transform, d3.zoomIdentity.scale(zoomLevel));
+    });
+
+    function zoomed(event) {
+        d3.select(chartSelector + ' .chart-container')
+        .attr('transform', event.transform)
+        .attr('transform-origin', 'center'); 
+}
 
     d3.select('#chargeSlider').on('input', function () {
         chargeStrength = +this.value; 
@@ -27,22 +47,23 @@ function createChart(chartSelector, dataURL, width, height, centerPos) {
                 .attr('y1', d => d.source.y)
                 .attr('x2', d => d.target.x)
                 .attr('y2', d => d.target.y)
-                .attr('stroke-width', 2);
+                .attr('stroke-width', 2)
         }
 
         function updateNodes() {
+        
             var nodeGroups = d3.select(chartSelector + ' .nodes')
                 .selectAll('g')
                 .data(nodes)
                 .join('g')
-                .attr('transform', d => 'translate(' + d.x + ',' + d.y + ') scale(0.7)');
-
+                .attr('transform', d => 'translate(' + d.x + ',' + d.y + ') scale(' + 0.7 + ')');
+        
             nodeGroups.selectAll('circle')
                 .data(d => [d])
                 .join('circle')
                 .attr('r', 30)
                 .attr('fill', 'cadetblue');
-
+        
             nodeGroups.selectAll('text')
                 .data(d => [d])
                 .join('text')
@@ -51,7 +72,7 @@ function createChart(chartSelector, dataURL, width, height, centerPos) {
                 .attr('dy', 5)
                 .attr('fill', '#666');
         }
-
+        
         function ticked() {
             updateLinks();
             updateNodes();
@@ -61,5 +82,22 @@ function createChart(chartSelector, dataURL, width, height, centerPos) {
     function updateChargeStrength(chargeStrength) {
         simulation.force('charge', d3.forceManyBody().strength(chargeStrength));
         simulation.alpha(0.3).restart(); 
+    }
+
+    d3.select('#weightSlider').on('input', function () {
+        var weightThreshold = +this.value-1;
+        d3.select('#weightValue').text(weightThreshold);
+        selectOnWeight(weightThreshold);
+    });
+
+    function selectOnWeight(weightThreshold) {
+        var links = d3.selectAll('line');
+        links.style('stroke', function (d) {
+            if (d.value > weightThreshold) {
+                return 'black';
+            } else {
+                return 'lightgray';
+            }
+        });
     }
 }
